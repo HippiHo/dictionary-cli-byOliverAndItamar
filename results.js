59ade442d4a9c2b9dd8a20618e41f04a3647ef5c
Elise Maschke <elise.maschke@outlook.de>
2019-03-21 14:21:38 +0100

    first commit

---
+++ results.js
@@ -0,0 +1,158 @@
+let results = [
+  {
+    id: "hello",
+    language: "en",
+    lexicalEntries: [
+      {
+        entries: [
+          {
+            etymologies: [
+              "early 19th century: variant of earlier hollo; related to holla"
+            ],
+            homographNumber: "000",
+            senses: [
+              {
+                definitions: [
+                  "used as a greeting or to begin a telephone conversation"
+                ],
+                examples: [{ text: "hello there, Katie!" }],
+                id: "m_en_gbus0460730.012",
+                short_definitions: ["used as greeting"],
+                subsenses: [
+                  {
+                    definitions: ["used to express surprise"],
+                    examples: [{ text: "hello, what's all this then?" }],
+                    id: "m_en_gbus0460730.017",
+                    regions: ["British"],
+                    short_definitions: ["used to express surprise"]
+                  },
+                  {
+                    definitions: [
+                      "used as a cry to attract someone's attention"
+                    ],
+                    examples: [{ text: "‘Hello below!’ he cried" }],
+                    id: "m_en_gbus0460730.018",
+                    short_definitions: ["used attract attention"]
+                  },
+                  {
+                    definitions: [
+                      "used informally to express sarcasm or anger"
+                    ],
+                    examples: [
+                      {
+                        text: "Hello! Did you even get what the play was about?"
+                      }
+                    ],
+                    id: "m_en_gbus0460730.019",
+                    short_definitions: [
+                      "used informally to express sarcasm or anger"
+                    ]
+                  }
+                ]
+              }
+            ],
+            variantForms: [{ text: "hallo" }, { text: "hullo" }]
+          }
+        ],
+        language: "en",
+        lexicalCategory: "Interjection",
+        pronunciations: [
+          {
+            audioFile:
+              "http://audio.oxforddictionaries.com/en/mp3/hello_gb_1.mp3",
+            dialects: ["British English"],
+            phoneticNotation: "IPA",
+            phoneticSpelling: "həˈləʊ"
+          },
+          {
+            dialects: ["British English"],
+            phoneticNotation: "IPA",
+            phoneticSpelling: "hɛˈləʊ"
+          }
+        ],
+        text: "hello"
+      },
+      {
+        entries: [
+          {
+            grammaticalFeatures: [{ text: "Singular", type: "Number" }],
+            homographNumber: "001",
+            senses: [
+              {
+                definitions: ["an utterance of ‘hello’; a greeting"],
+                examples: [
+                  { text: "she was getting polite nods and hellos from people" }
+                ],
+                id: "m_en_gbus0460730.025",
+                short_definitions: ["utterance of ‘hello’"],
+                thesaurusLinks: [
+                  {
+                    entry_id: "acknowledgement",
+                    sense_id: "t_en_gb0000184.002"
+                  }
+                ]
+              }
+            ],
+            variantForms: [{ text: "hullo" }, { text: "hallo" }]
+          }
+        ],
+        language: "en",
+        lexicalCategory: "Noun",
+        pronunciations: [
+          {
+            dialects: ["British English"],
+            phoneticNotation: "IPA",
+            phoneticSpelling: "hɛˈləʊ"
+          },
+          {
+            audioFile:
+              "http://audio.oxforddictionaries.com/en/mp3/hello_gb_1.mp3",
+            dialects: ["British English"],
+            phoneticNotation: "IPA",
+            phoneticSpelling: "həˈləʊ"
+          }
+        ],
+        text: "hello"
+      },
+      {
+        entries: [
+          {
+            grammaticalFeatures: [
+              { text: "Intransitive", type: "Subcategorization" },
+              { text: "Present", type: "Tense" }
+            ],
+            homographNumber: "002",
+            senses: [
+              {
+                definitions: ["say or shout ‘hello’"],
+                examples: [{ text: "I pressed the phone button and helloed" }],
+                id: "m_en_gbus0460730.034",
+                short_definitions: ["say or shout ‘hello’"]
+              }
+            ],
+            variantForms: [{ text: "hullo" }, { text: "hallo" }]
+          }
+        ],
+        language: "en",
+        lexicalCategory: "Verb",
+        pronunciations: [
+          {
+            audioFile:
+              "http://audio.oxforddictionaries.com/en/mp3/hello_gb_1.mp3",
+            dialects: ["British English"],
+            phoneticNotation: "IPA",
+            phoneticSpelling: "həˈləʊ"
+          },
+          {
+            dialects: ["British English"],
+            phoneticNotation: "IPA",
+            phoneticSpelling: "hɛˈləʊ"
+          }
+        ],
+        text: "hello"
+      }
+    ],
+    type: "headword",
+    word: "hello"
+  }
+];
